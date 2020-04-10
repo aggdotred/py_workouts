96593801d00d41df11a08401b84c54287ed26ade
Adan Gutierrez <hevyw8@gmail.com>
2020-04-10 15:46:25 -0500

    added error handling and interface

--- my_sum/README.md
+++ my_sum/README.md
@@ -1,1 +1,3 @@
 The challenge here is to write a mysum function that does the same thing as the built-in sum function. However, instead of taking a single sequence as a parameter, it should take a variable number of arguments. Thus while we might invoke sum([1,2,3]), we would instead invoke mysum(1,2,3) or mysum(10,20,30,40,50).
+
+TODO: handle number gathering
--- my_sum/my_sum.py
+++ my_sum/my_sum.py
@@ -1,6 +1,4 @@
 
-
-
 def mysum(*args):
     acc = 0
     for i in args:
@@ -8,8 +6,36 @@ def mysum(*args):
     return acc
 
 
-print(mysum(1))
+def get_numbers():
+    return input("List the numbers to be added separated by a comma: ")
 
-print(mysum(1, 2))
 
-print(mysum(1, 2, 3))
+def parse_num_string(a_string_of_numbers):
+    return a_string_of_numbers.split(',')
+
+
+def convert_num_string(a_list_of_nums_as_strings):
+    num_list = []
+    for i in range(0, len(a_list_of_nums_as_strings)):
+        try:
+            num_list.append(int(a_list_of_nums_as_strings[i]))
+        except:
+            print("Please enter numbers separated by commas")
+    return num_list
+
+
+def add_numbers(a_list_of_numbers):
+    acc = 0
+    for i in range(0, len(a_list_of_numbers)):
+        acc += a_list_of_numbers[i]
+    return acc
+
+
+num_string = get_numbers()
+num_string_list = parse_num_string(num_string)
+conv_num_list = convert_num_string(num_string_list)
+print(add_numbers(conv_num_list))
+
+# NOTE input would be better handled by passing args from the cli
+# NOTE the original exercise intended to use the splat operator. //
+# // but for passing input, parsing out a list makes more sense
