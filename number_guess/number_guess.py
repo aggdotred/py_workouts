fd2c9c4e7846d53770cdc1033ea6ca34bd6744b2
Adan Gutierrez <hevyw8@gmail.com>
2020-04-09 23:44:17 -0500

    added for simple for loop for turn counting

--- number_guess/number_guess.py
+++ number_guess/number_guess.py
@@ -1,27 +1,20 @@
 import random
+answer = random.randint(0, 100)
 
-secret_number = random.randint(0, 100)
-
-
-def get_guess():
+for i in range(0, 3):
+    i += 1
     try:
-        guess = int(input("What's your guess? "))
-        check_guess(guess)
+        user_guess = int(input("What's your guess? "))
     except:
         print("Please enter a whole number not a string.")
-        get_guess()
+        break
 
+    if user_guess == answer:
+        print(f"Right!  The answer is {user_guess}")
+        break
 
-def check_guess(guess_int):
-    if guess_int == secret_number:
-        print("You win!")
-        return
-    elif guess_int > secret_number:
-        print("Too high!")
-        new_guess = get_guess()
-    else:
-        print("Too low!")
-        new_guess = get_guess()
+    elif user_guess < answer:
+        print(f"Your guess of {user_guess} is too low!")
 
-
-get_guess()
+    elif user_guess > answer:
+        print(f"Your guess of {user_guess} is too high!")
